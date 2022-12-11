cb53ead6380ad30f597bf2d29108797bf78b379e
titicuevas <enrique.cuevas@iesdonana.org>
2022-12-11 12:10:12 +0100

    Actualizada

---
+++ tienda.sql
@@ -0,0 +1,49 @@
+-- Active: 1670516977612@@127.0.0.1@5432@prueba
+DROP TABLE IF EXISTS articulos CASCADE;
+
+CREATE TABLE articulos (
+    id          bigserial     PRIMARY KEY,
+    codigo      varchar(13)   NOT NULL UNIQUE,
+    descripcion varchar(255)  NOT NULL,
+    precio      numeric(7, 2) NOT NULL
+);
+
+DROP TABLE IF EXISTS usuarios CASCADE;
+
+CREATE TABLE usuarios (
+    id       bigserial    PRIMARY KEY,
+    usuario  varchar(255) NOT NULL UNIQUE,
+    password varchar(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS facturas CASCADE;
+
+CREATE TABLE facturas (
+    id         bigserial  PRIMARY KEY,
+    created_at timestamp  NOT NULL DEFAULT localtimestamp(0),
+    usuario_id bigint NOT NULL REFERENCES usuarios (id)
+);
+
+DROP TABLE IF EXISTS articulos_facturas CASCADE;
+
+CREATE TABLE articulos_facturas (
+    articulo_id bigint NOT NULL REFERENCES articulos (id),
+    factura_id  bigint NOT NULL REFERENCES facturas (id),
+    cantidad    int    NOT NULL,
+    PRIMARY KEY (articulo_id, factura_id)
+);
+
+-- Carga inicial de datos de prueba:
+
+INSERT INTO articulos (codigo, descripcion, precio)
+    VALUES ('18273892389', 'Yogur piña', 200.50),
+           ('83745828273', 'Tigretón', 50.10),
+           ('51736128495', 'Disco duro SSD 500 GB', 150.30),
+           ('83746828273', 'Tigretón', 50.10),
+           ('51786128435', 'Disco duro SSD 500 GB', 150.30),
+           ('83745228673', 'Tigretón', 50.10),
+           ('51786198495', 'Disco duro SSD 500 GB', 150.30);
+
+INSERT INTO usuarios (usuario, password)
+    VALUES ('admin', crypt('admin', gen_salt('bf', 10))),
+           ('pepe', crypt('pepe', gen_salt('bf', 10)));
